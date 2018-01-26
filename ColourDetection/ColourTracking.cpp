7af7874d2d5de85beba4edcd64b2d855522eaa90
KyleJosling <kylejosling@hotmail.com>
2018-01-26 05:02:04 +0000

    Added all three PIDs

--- ColourDetection/ColourTracking.cpp
+++ ColourDetection/ColourTracking.cpp
@@ -37,7 +37,7 @@ int main(int argc, char** argv){
 	fcu::FlightController fcu(device,baudrate);
 	fcu.initialise();
 	fcu.reboot();
-	armFlightController(&fcu);
+	//armFlightController(&fcu);
 
 	//Videocapture object
 	VideoCapture cap(0);
@@ -46,16 +46,21 @@ int main(int argc, char** argv){
 	cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
 	cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
 
-	//Declare PID controller
+	//Declare PID controllers
 	//( double dt, double max, double min, double Kp, double Kd, double Ki );
 	PID yawPid = PID(0.1,500,-500,0.702,4.9,0.00006);
+	PID pitchPid = PID(0.1,500,-500,0.702,4.9,0.00006);
+	PID throttlePid = PID (0.1,500,-500,0.702,4.9,0.00006);
 
 	//The set variable is half the width of the window
-	int sVar=320;
+	int yawSVar=320;
+	int throttleSVar=240;
 	//The process variable is the location of the face in the frame
-	int pVar=0;
+	int yawPVar=0;
+	int throttlePVar=0;
 	//Calculated output
 	double yawOutput;
+	double throttleOutput;
 
 	int frameCounter=0;
 	Mat frame;
@@ -64,12 +69,30 @@ int main(int argc, char** argv){
   {
 		//Get a frame
 		cap >> frame;
-
-		detectedPoint=detectObject(frame,100,50,50);
+		//~ int hue;
+		//~ int sat;
+		//~ int val;
+		//~ std::cin >> hue;
+		//~ std::cin >> sat;
+		//~ std::cin >>val;
+		detectedPoint=detectObject(frame,100,60,60);
 
 		//Set process variable
-		pVar=detectedPoint.pt.x;
-		yawOutput=(yawPid.calculate(sVar,pVar)+1500);
+		yawPVar=detectedPoint.pt.x;
+		throttlePVar=detectedPoint.pt.y;
+		if (yawPVar!=0){
+			yawOutput=(yawPid.calculate(yawSVar,yawPVar)+1500);
+		}
+		else{
+		yawOutput=1500;
+		}
+		if (throttlePVar!=0)
+		{
+			throttleOutput=(throttlePid.calculate(throttleSVar,throttlePVar)+1000);
+		}
+		else{
+			throttleOutput=1200;
+		}
 		fcu.setRc(1500, 1500, yawOutput, 1200, 1000, 1000, 1000, 1000);
 		//Output
 		std::cout << yawOutput << std::endl;
@@ -78,7 +101,7 @@ int main(int argc, char** argv){
 		//Increment the frame counter
 		frameCounter++;
 		std::cout << "Counter: " << frameCounter << std::endl;
-
+		
 		int c = waitKey(10);
     if( (char)c == 'c' ) { break; }
 
@@ -105,7 +128,7 @@ obj_point detectObject(Mat frame, int hue, int sat, int val){
 	cvtColor( frame, frame_hsv, CV_BGR2HSV );
 
 	//Mask
-	cv::inRange(frame_hsv,Scalar(hue-10,sat,val),Scalar(hue+10,255,255),frame_threshold);
+	cv::inRange(frame_hsv,Scalar(hue-7,sat,val),Scalar(hue+7,255,255),frame_threshold);
 
 	cv::erode(frame_threshold, frame_threshold, erode_rect,Point(-1,-1),2);
 	cv::dilate(frame_threshold, frame_threshold, dilate_rect, Point(-1,-1), 2);
@@ -138,7 +161,7 @@ obj_point detectObject(Mat frame, int hue, int sat, int val){
 	}
 
 	// If no contours found then mean point is 0
-	if(largest_contour_area == 0)
+	if(largest_contour_area ==0 || largest_contour_area<800)
 	{
 		objectPoint.pt= Point2f(0,0);
 		objectPoint.size=0;
